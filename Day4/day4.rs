3326d1ebc181a1f4b9e3c838595e53ff6ff10684
Andrew <Andrew.Hutcheson@gmail.com>
2022-12-27 18:47:42 -0600

    day 4 update

--- Day4/day4.rs
+++ Day4/day4.rs
@@ -13,9 +13,11 @@ fn main(){
     let mut digit1: i32;
     let mut digit2: i32;
     let mut digit3: i32;
-    let mut counter = 0;
+    let mut counter_part1 = 0;
+    let mut counter_part2 = 0;
     let mut is_increasing: bool;
     let mut is_double_adjacent_only: bool;
+    let mut is_double_adjacent: bool;
     let mut previous_digit: i32;
     let mut position: i8;
 
@@ -35,32 +37,7 @@ fn main(){
     while index <= max {
         current_num = index;
         is_double_adjacent_only = false;
-        is_increasing = true;
-        while current_num > 10 {
-            digit1 = current_num % 10;
-            digit2 = current_num / 10;
-            digit2 = digit2 % 10;
-            current_num = current_num / 10;
-            if digit1 == digit2 {
-                is_double_adjacent_only = true;
-            }
-            if digit2 > digit1{
-                is_increasing = false;
-            }
-        }
-        if is_double_adjacent_only && is_increasing {
-            counter += 1;
-        }
-        index += 1;
-    }
-
-    println!("Part 1: {}", counter);
-
-    counter = 0;
-    index = min;
-    while index <= max {
-        current_num = index;
-        is_double_adjacent_only = false;
+        is_double_adjacent = false;
         is_increasing = true;
         previous_digit = current_num % 10;
         position = 0;
@@ -71,8 +48,14 @@ fn main(){
             digit2 = digit2 % 10;
             digit3 = current_num / 100;
             digit3 = digit3 % 10;
-            current_num = current_num / 10;          
+            current_num = current_num / 10;  
             
+            //part1
+            if digit1 == digit2 {
+                is_double_adjacent = true;
+            }
+            
+            //part2
             if (digit1 == digit2) && (digit1 != digit3) {
                 if position == 1 {
                     is_double_adjacent_only = true;
@@ -88,10 +71,15 @@ fn main(){
 
             previous_digit = digit1;
         }
+        if is_double_adjacent && is_increasing {
+            counter_part1 += 1;
+        }
         if is_double_adjacent_only && is_increasing {
-            counter += 1;
+            counter_part2 += 1;
         }
         index += 1;
     }
-    println!("Part 2: {}", counter); //not 174, not 163 or 263
+
+    println!("Part 1: {}", counter_part1); //not 174, not 163 or 263
+    println!("Part 2: {}", counter_part2); //not 174, not 163 or 263
 }
